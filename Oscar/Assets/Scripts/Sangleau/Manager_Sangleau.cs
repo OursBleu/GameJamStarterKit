3ece4362ddd617fcc2f97030df7f6e3a986a8ddb
Erwan Giry-Fouquet <erwangf@gmail.com>
2015-10-09 18:36:04 +0400

    IA Sangleau v0.1
    Ajout de l'IA Sangleau, en version 0.1.
    Fonctionnalités :
    - Courir vers le joueur (détecté par le tag "Player")
    - Ne rien faire si je joueur n'est pas à portée
    Stats :
    - PV : 2
    - Vitesse : 120
    - Saut : 80

---
+++ Oscar/Assets/Scripts/Sangleau/Manager_Sangleau.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+    Nom : Manager Sangleau
+    Version : 0.1
+    Créé par : Erwan Giry-Fouquet
+    Description : 
+    | IA (Manager) permettant à un sangleau de se déplacer vers le joueur, de le mordre,
+    | et se de se repérer dans l'environnement
+    Status : OK
+*/
+public class Manager_Sangleau : Manager, IManagerInput
+{
+    Transform playerTransform;
+
+    //range of détection
+    private float _range = 10f;
+    void Start()
+    {
+        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+    void Update()
+    {
+        //Si le joueur est à portée de détection, se diriger vers lui, sinon ne pas bouger
+        if (distPlayer() < _range)
+        {
+            _direction = dirPlayer();
+        }
+        else _direction = Vector2.zero;
+
+    }
+
+    private Vector2 _direction= Vector2.zero;
+    public Vector2 Direction
+    {
+        get
+        {
+            return _direction;
+        }
+    }
+
+  
+    public bool this[int index]
+    {
+        get
+        {
+            switch (index)
+            {
+                case 0:
+                    // 0 est une touche reservée pour le saut, retourner true quand le personnage veut sauter.
+                    return false;
+                case 1:
+                    // Ex : si la cible est a portée d'attaque : return true, sinon : return false;
+                    return false;
+                case 2:
+                    return false;
+                case 3:
+                    return false;
+                default:
+                    // ne pas changer ce qui est écrit dans le cas par défaut et ne rien rajouter
+                    return false;
+            }
+        }
+    }
+
+    Vector2 vectPlayer()
+    {
+        return (playerTransform.position - gameObject.transform.position).AsVector2();
+    }
+
+    Vector2 dirPlayer()
+    {
+        return vectPlayer().normalized;
+    }
+
+    float distPlayer()
+    {
+        return vectPlayer().magnitude;
+    }
+}
+
