32397430ebc96d00de604eb6069ad0ff07031bd0
OursBleu <marc.garbage@gmail.com>
2015-10-12 15:58:28 +0400

    Commit pour Github desktop

---
+++ Oscar/Assets/Scripts/Librairie fsm/States/StateMovingCurvedGround.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StateMovingCurvedGround : State
+{
+    ManagerLocomotion _locomotion;
+    ManagerGroundProbe _ground;
+    IManagerInput _input;
+
+    public StateMovingCurvedGround(Fsm fsm) : base(fsm)
+    {
+        _locomotion = Fsm.gameObject.GetOrAdd<ManagerLocomotion>();
+        _ground = Fsm.gameObject.GetOrAdd<ManagerGroundProbe>();
+        _input = Fsm.gameObject.GetComponent<IManagerInput>();
+    }
+
+    public override void StateEnter()
+    {
+
+    }
+
+    public override void StateUpdate()
+    {
+        RaycastHit2D groundProbe = _ground.GroundProbe;
+
+        // MOVE
+
+        Vector2 direction = _input.Direction.x * Fsm.transform.right;
+        Fsm.transform.up = groundProbe.normal;
+        _locomotion.Move(direction);
+
+        // X SCALE INVERSION
+
+        Vector3 scale = Fsm.transform.localScale;
+        if ((direction.x < 0f && scale.x > 0f) || (direction.x > 0f && scale.x < 0f)) scale.x *= -1;
+        Fsm.transform.localScale = scale;
+
+        // GROUND HUGGING /!\ bug : se colle au plafond et aux murs quasi-verticaux, se clipse trop tôt aux pentes
+
+        Collider2D col = Fsm.GetComponent<Collider2D>();
+        if (_ground.IsCloseToGround && _input.Direction != Vector2.zero)
+        {
+            Fsm.transform.position = groundProbe.point + (col.bounds.extents.y * groundProbe.normal);
+        }
+    }
+
+    public override void StateExit()
+    {
+        Fsm.transform.up = Vector3.up;
+    }
+
+}
---
+++ Oscar/Assets/Scripts/Oscar/InstantiableFireball.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstantiableFireball : Instantiable
+{
+    private ManagerLocomotion _locomotion;
+    private ManagerCollision _collision;
+
+    void Awake()
+    {
+        _locomotion = gameObject.GetOrAdd<ManagerLocomotion>();
+        _collision = gameObject.GetOrAdd<ManagerCollision>();
+
+        _locomotion.DefaultSpeed = 300f;
+        Grid.particleSystem.Play("ball", transform, transform.right, true);
+    }
+
+    void Update()
+    { 
+        ManagerCollision launcherCol = Launcher.GetComponent<ManagerCollision>();
+        if (launcherCol) _collision.TeamIndex = launcherCol.TeamIndex;
+
+        _locomotion.Move(transform.right);
+
+        if (_collision.IsColliding && (!_collision.HasOtherACollisionManager || _collision.IsOtherInDifferentTeam))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnDestroy()
+    {
+        Grid.particleSystem.Play("explosion", transform.position);
+    }
+
+    
+   
+}
